7636e804ab513a9616fb0e7ea05617da77f61b3f
SharidMadrid <75934463+SharidMadrid@users.noreply.github.com>
2022-03-31 16:49:47 -0500

    Add files via upload

---
+++ controlador.js
@@ -0,0 +1,23 @@
+document.getElementByI('formulario-inicio').addEventListener('submit', function (){
+    const usuario = document.getElementById("usuario").values();
+    const clave = document.getElementById("clave").values();
+    const rol = document.getElementById("rol").values();
+
+     console.log(usuario, clave, rol);
+});
+function inicioSesion(){
+
+    switch (rol){
+        case 1:
+            //href="ap_liderProyecto.html"
+            break;
+        case 2:
+           // href="ap_Estudiante.html"
+            break;
+        case 3:
+           // href="ap_Asesor.html"
+            break;
+    }
+
+    console.log(documento.getElementById('usuario').value);
+}
